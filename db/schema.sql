14f71ed7bc454153d5015d4dd8659106c8880883
Jonhe <Jonhenrylangerman@gmail.com>
2021-09-12 20:52:38 -0700

    mvp/readme

--- db/schema.sql
+++ db/schema.sql
@@ -4,3 +4,11 @@ DROP DATABASE IF EXISTS ecommerce_db;
 -- CREATE DATABASE
 CREATE DATABASE ecommerce_db;
 
+/*
+--mysql -u root -p
+--source db/schema.sql
+--npm run seed
+--http://localhost:3001/api/tags/
+--http://localhost:3001/api/products/
+--http://localhost:3001/api/categories/
+*/
